fdb12ace3c1119c77f459a5ae1ce7b6ad47b0dfa
Ben Heidemann <ben@heidemann.dev>
2024-03-17 12:59:23 +0000

    feat: add ntfs to supported filesystems

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Filesystems
+  boot.supportedFilesystems = [ "ntfs" ];
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
